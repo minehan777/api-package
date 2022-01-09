d0182924582767302dee5068c622956b091203cb
takuyaFujimoto <97405065+minehan777@users.noreply.github.com>
2022-01-10 04:38:02 +0900

    検証中

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
-from src.db_model import *
+from db_model import test_function
+from db_model.test_class import *
+# a = TestClass(1,'hoge')
+# print(test_function.hoge())
+# print(a.id)
+# print(a.name)

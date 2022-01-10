c36998cdfb8c17cbef05b4a1f0746c1eb39dc968
takuyaFujimoto <97405065+minehan777@users.noreply.github.com>
2022-01-10 22:58:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+
 from setuptools import setup
 from setuptools import find_packages
 
@@ -9,6 +14,7 @@ setup(
     description='',
     packages=find_packages("src"),
     package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires=[],
     extras_require={
         'develop' : [],
